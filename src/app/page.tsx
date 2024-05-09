f92b928a8f46d2ad7728c27bf3f80ea0d8f070fb
Samantha Waters <101077786+samantha-waters@users.noreply.github.com>
2024-05-09 15:18:24 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import '../styles/styles.css';
 
 export default function Cocktails() {
